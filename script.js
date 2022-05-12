3dc04f657b1a251ea08fa89eeb1925d4600cedf6
Anna <annabutkova94@gmail.com>
2022-05-12 23:20:44 +0500

    feat: language switch

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const body = document.getElementById('root');
+const textArea = document.createElement('textarea');
 function showTextArea() {
-  const textArea = document.createElement('textarea');
   textArea.classList.add('textarea');
   const note = document.createElement('p');
-  note.textContent = 'Клавиатура создана в операционной системе Windows. Для переключения языка комбинация: левыe ctrl + alt';
+  note.innerHTML = 'Клавиатура создана в ОС Windows.<br> Для переключения языка комбинация: левыe ctrl + alt';
   note.classList.add('note');
   body.append(note, textArea);
 }
 
-const KEYS = [{ ru: 'ё', en: '`' }, { ru: '1', en: '1' }, { ru: '2', en: '2' }, { ru: '3', en: '3' }, { ru: '4', en: '4' }, { ru: '5', en: '5' }, { ru: '6', en: '6' }, { ru: '7', en: '7' }, { ru: '8', en: '8' }, { ru: '9', en: '9' }, { ru: '0', en: '0' }, { ru: '-', en: '-' }, { ru: '=', en: '=' }, { ru: 'backspace', en: 'backspace' }, /*  */ { ru: 'tab', en: 'tab' }, { ru: 'й', en: 'q' }, { ru: 'ц', en: 'w' }, { ru: 'у', en: 'e' }, { ru: 'к', en: 'r' }, { ru: 'е', en: 't' }, { ru: 'н', en: 'y' }, { ru: 'г', en: 'u' }, { ru: 'ш', en: 'i' }, { ru: 'щ', en: 'o' }, { ru: 'з', en: 'p' }, { ru: 'х', en: '[' }, { ru: 'ъ', en: ']' }, { ru: '\\', en: '\\' }, { ru: 'del', en: 'del' }, /*  */ { ru: 'capslock', en: 'capslock' }, { ru: 'ф', en: 'a' }, { ru: 'ы', en: 's' }, { ru: 'в', en: 'd' }, { ru: 'а', en: 'f' }, { ru: 'п', en: 'g' }, { ru: 'р', en: 'h' }, { ru: 'о', en: 'j' }, { ru: 'л', en: 'k' }, { ru: 'д', en: 'l' }, { ru: 'ж', en: ';' }, { ru: 'э', en: '\'' }, { ru: 'enter', en: 'enter' }, /*  */ { ru: 'shift', en: 'shift' }, { ru: 'я', en: 'z' }, { ru: 'ч', en: 'x' }, { ru: 'с', en: 'c' }, { ru: 'м', en: 'v' }, { ru: 'и', en: 'b' }, { ru: 'т', en: 'n' }, { ru: 'ь', en: 'm' }, { ru: 'б', en: ',' }, { ru: 'ю', en: '.' }, { ru: '.', en: '/' }, { ru: '▲', en: '▲' }, { ru: 'shift', en: 'shift' }, /*  */ { ru: 'ctrl', en: 'ctrl' }, { ru: 'win', en: 'win' }, { ru: 'alt', en: 'alt' }, { ru: 'space', en: 'space' }, { ru: 'alt', en: 'alt' }, { ru: '◄', en: '◄' }, { ru: '▼', en: '▼' }, { ru: '►', en: '►' }, { ru: 'ctrl', en: 'ctrl' }];
+const GENERALKEYS = ['backspace', 'tab', 'del', 'capslock', 'enter', 'shift', 'shift ', 'ctrl', 'win', 'alt', 'space'];
 
-const GeneralKeys = ['backspace', 'tab', 'del', 'enter', 'shift', 'shift ', 'ctrl', 'win', 'alt', 'space'];
+const KEYS = [{ ru: 'ё', en: '`' }, { ru: '1', en: '1' }, { ru: '2', en: '2' }, { ru: '3', en: '3' }, { ru: '4', en: '4' }, { ru: '5', en: '5' }, { ru: '6', en: '6' }, { ru: '7', en: '7' }, { ru: '8', en: '8' }, { ru: '9', en: '9' }, { ru: '0', en: '0' }, { ru: '-', en: '-' }, { ru: '=', en: '=' }, { ru: 'backspace', en: 'backspace' }, /*  */ { ru: 'tab', en: 'tab' }, { ru: 'й', en: 'q' }, { ru: 'ц', en: 'w' }, { ru: 'у', en: 'e' }, { ru: 'к', en: 'r' }, { ru: 'е', en: 't' }, { ru: 'н', en: 'y' }, { ru: 'г', en: 'u' }, { ru: 'ш', en: 'i' }, { ru: 'щ', en: 'o' }, { ru: 'з', en: 'p' }, { ru: 'х', en: '[' }, { ru: 'ъ', en: ']' }, { ru: '\\', en: '\\' }, { ru: 'del', en: 'del' }, /*  */ { ru: 'capslock', en: 'capslock' }, { ru: 'ф', en: 'a' }, { ru: 'ы', en: 's' }, { ru: 'в', en: 'd' }, { ru: 'а', en: 'f' }, { ru: 'п', en: 'g' }, { ru: 'р', en: 'h' }, { ru: 'о', en: 'j' }, { ru: 'л', en: 'k' }, { ru: 'д', en: 'l' }, { ru: 'ж', en: ';' }, { ru: 'э', en: '\'' }, { ru: 'enter', en: 'enter' }, /*  */ { ru: 'shift', en: 'shift' }, { ru: 'я', en: 'z' }, { ru: 'ч', en: 'x' }, { ru: 'с', en: 'c' }, { ru: 'м', en: 'v' }, { ru: 'и', en: 'b' }, { ru: 'т', en: 'n' }, { ru: 'ь', en: 'm' }, { ru: 'б', en: ',' }, { ru: 'ю', en: '.' }, { ru: '.', en: '/' }, { ru: '▲', en: '▲' }, { ru: 'shift ', en: 'shift ' }, /*  */ { ru: 'ctrl', en: 'ctrl' }, { ru: 'win', en: 'win' }, { ru: 'alt', en: 'alt' }, { ru: 'space', en: 'space' }, { ru: 'alt', en: 'alt' }, { ru: '◄', en: '◄' }, { ru: '▼', en: '▼' }, { ru: '►', en: '►' }, { ru: 'ctrl', en: 'ctrl' }];
 
 const Keyboard = {
   elements: {
@@ -39,7 +39,6 @@ const Keyboard = {
     // Add to DOM
     this.elements.main.appendChild(this.elements.keysContainer);
 
-
     document.body.append(this.elements.main);
   },
 
@@ -67,44 +66,80 @@ const Keyboard = {
       keyElement.setAttribute('type', 'button');
       keyElement.setAttribute('data-value', `Key${(el.en).toUpperCase()}`);
       keyElement.classList.add('keyboard__key');
+      const insertLineBreak = ['backspace', 'del', 'enter', 'shift '].indexOf(key) !== -1;
 
+      GENERALKEYS.forEach((genkey) => {
+        if (key === genkey) {
+          keyElement.classList.add('general__key');
+        
+        }
+      });
       switch (key) {
         case 'backspace':
-          keyElement.classList.add('keyboard__key--wide');
-
+          keyElement.classList.add('wide2x');
+          keyElement.textContent = '←  Backspace';
           keyElement.addEventListener('click', () => {
             this.properties.value = this.properties.value
               .substring(0, this.properties.value.length - 1);
-            this.triggerEvent('oninput');
+            textArea.value = this.properties.value;
           });
 
           break;
 
         case 'capslock':
-          keyElement.classList.add('keyboard__key--wide', 'keyboard__key--activatable');
+          keyElement.classList.add('wide2x', '--activatable');
+          keyElement.textContent = key;
+
           keyElement.addEventListener('click', () => {
             this.toggleCapsLock();
-            keyElement.classList.toggle('keyboard__key--active', this.properties.capsLock);
+            keyElement.classList.toggle('--active', this.properties.capsLock);
           });
 
           break;
-
+        case 'shift':
+        case 'shift ':
+          keyElement.classList.add('wide2x');
+          keyElement.textContent = key;
+          keyElement.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
+          break;
+        case 'del':
+        case 'alt':
+        case 'ctrl':
+        case 'win':
+        case '◄':
+        case '▲':
+        case '►':
+        case '▼':
+          keyElement.textContent = key;
+          keyElement.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
+          break;
+        case 'tab':
+          keyElement.textContent = key;
+          keyElement.addEventListener('click', () => {
+            this.properties.value += '  ';
+            textArea.value = this.properties.value;
+          });
+          break;
         case 'enter':
-          keyElement.classList.add('keyboard__key--wide');
-
+          keyElement.classList.add('wide2x');
+          keyElement.textContent = key;
           keyElement.addEventListener('click', () => {
             this.properties.value += '\n';
-            this.triggerEvent('oninput');
+            textArea.value = this.properties.value;
           });
 
           break;
 
         case 'space':
-          keyElement.classList.add('keyboard__key--extra-wide');
+          keyElement.classList.add('space');
 
           keyElement.addEventListener('click', () => {
             this.properties.value += ' ';
-            this.triggerEvent('oninput');
+            textArea.value = this.properties.value;
           });
 
           break;
@@ -116,33 +151,32 @@ const Keyboard = {
             this.properties.value += this.properties.capsLock
               ? key.toUpperCase()
               : key.toLowerCase();
-            this.triggerEvent('oninput');
+            textArea.value = this.properties.value;
           });
 
           break;
       }
 
       keyboardWrapper.appendChild(keyElement);
+      if (insertLineBreak) {
+        keyboardWrapper.appendChild(document.createElement('br'));
+      }
     });
     return keyboardWrapper;
   },
-  triggerEvent(handlerName) {
-    if (typeof this.eventHandlers[handlerName] === 'function') {
-      this.eventHandlers[handlerName](this.properties.value);
-    }
-  },
+
   toggleCapsLock() {
     this.properties.capsLock = !this.properties.capsLock;
 
     // eslint-disable-next-line no-restricted-syntax
     for (const key of this.elements.keys) {
-      for (let i = 0; i < GeneralKeys.length; i += 1) {
-        if (key.childElementCount === 0) {
+      GENERALKEYS.forEach((genkey) => {
+        if (genkey.indexOf(key.textContent.toLowerCase()) === -1) {
           key.textContent = this.properties.capsLock
             ? key.textContent.toUpperCase()
             : key.textContent.toLowerCase();
         }
-      }
+      });
     }
   },
 };
