5ca639c38dbf4cce4e2fe04ff3a16ce475ec6552
Anna <annabutkova94@gmail.com>
2022-05-13 01:04:16 +0500

    feat: lightning by typing

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const textArea = document.createElement('textarea');
 function showTextArea() {
   textArea.classList.add('textarea');
   const note = document.createElement('p');
-  note.innerHTML = 'Клавиатура создана в ОС Windows.<br> Для переключения языка комбинация: левыe ctrl + alt';
+  note.innerHTML = 'Клавиатура создана в ОС Windows.<br> Для переключения языка комбинация: левыe Ctrl + Alt';
   note.classList.add('note');
   body.append(note, textArea);
 }
 
-const GENERALKEYS = ['backspace', 'tab', 'del', 'capslock', 'enter', 'shift', 'shift ', 'ctrl', 'win', 'alt', 'space'];
+const GENERALKEYS = ['Backspace', 'Tab', 'Delete', 'CapsLock', 'Enter', 'Shift', 'Shift ', 'Control', 'OS', 'Alt', 'space', 'ArrowDown', 'ArrowRight', 'ArrowUp', 'ArrowLeft'];
 
-const KEYS = [{ ru: 'ё', en: '`' }, { ru: '1', en: '1' }, { ru: '2', en: '2' }, { ru: '3', en: '3' }, { ru: '4', en: '4' }, { ru: '5', en: '5' }, { ru: '6', en: '6' }, { ru: '7', en: '7' }, { ru: '8', en: '8' }, { ru: '9', en: '9' }, { ru: '0', en: '0' }, { ru: '-', en: '-' }, { ru: '=', en: '=' }, { ru: 'backspace', en: 'backspace' }, /*  */ { ru: 'tab', en: 'tab' }, { ru: 'й', en: 'q' }, { ru: 'ц', en: 'w' }, { ru: 'у', en: 'e' }, { ru: 'к', en: 'r' }, { ru: 'е', en: 't' }, { ru: 'н', en: 'y' }, { ru: 'г', en: 'u' }, { ru: 'ш', en: 'i' }, { ru: 'щ', en: 'o' }, { ru: 'з', en: 'p' }, { ru: 'х', en: '[' }, { ru: 'ъ', en: ']' }, { ru: '\\', en: '\\' }, { ru: 'del', en: 'del' }, /*  */ { ru: 'capslock', en: 'capslock' }, { ru: 'ф', en: 'a' }, { ru: 'ы', en: 's' }, { ru: 'в', en: 'd' }, { ru: 'а', en: 'f' }, { ru: 'п', en: 'g' }, { ru: 'р', en: 'h' }, { ru: 'о', en: 'j' }, { ru: 'л', en: 'k' }, { ru: 'д', en: 'l' }, { ru: 'ж', en: ';' }, { ru: 'э', en: '\'' }, { ru: 'enter', en: 'enter' }, /*  */ { ru: 'shift', en: 'shift' }, { ru: 'я', en: 'z' }, { ru: 'ч', en: 'x' }, { ru: 'с', en: 'c' }, { ru: 'м', en: 'v' }, { ru: 'и', en: 'b' }, { ru: 'т', en: 'n' }, { ru: 'ь', en: 'm' }, { ru: 'б', en: ',' }, { ru: 'ю', en: '.' }, { ru: '.', en: '/' }, { ru: '▲', en: '▲' }, { ru: 'shift ', en: 'shift ' }, /*  */ { ru: 'ctrl', en: 'ctrl' }, { ru: 'win', en: 'win' }, { ru: 'alt', en: 'alt' }, { ru: 'space', en: 'space' }, { ru: 'alt', en: 'alt' }, { ru: '◄', en: '◄' }, { ru: '▼', en: '▼' }, { ru: '►', en: '►' }, { ru: 'ctrl', en: 'ctrl' }];
+const KEYS = [{ ru: 'ё', en: '`' }, { ru: '1', en: '1' }, { ru: '2', en: '2' }, { ru: '3', en: '3' }, { ru: '4', en: '4' }, { ru: '5', en: '5' }, { ru: '6', en: '6' }, { ru: '7', en: '7' }, { ru: '8', en: '8' }, { ru: '9', en: '9' }, { ru: '0', en: '0' }, { ru: '-', en: '-' }, { ru: '=', en: '=' }, { ru: 'Backspace', en: 'Backspace' }, /*  */ { ru: 'Tab', en: 'Tab' }, { ru: 'й', en: 'q' }, { ru: 'ц', en: 'w' }, { ru: 'у', en: 'e' }, { ru: 'к', en: 'r' }, { ru: 'е', en: 't' }, { ru: 'н', en: 'y' }, { ru: 'г', en: 'u' }, { ru: 'ш', en: 'i' }, { ru: 'щ', en: 'o' }, { ru: 'з', en: 'p' }, { ru: 'х', en: '[' }, { ru: 'ъ', en: ']' }, { ru: '\\', en: '\\' }, { ru: 'Delete', en: 'Delete' }, /*  */ { ru: 'CapsLock', en: 'CapsLock' }, { ru: 'ф', en: 'a' }, { ru: 'ы', en: 's' }, { ru: 'в', en: 'd' }, { ru: 'а', en: 'f' }, { ru: 'п', en: 'g' }, { ru: 'р', en: 'h' }, { ru: 'о', en: 'j' }, { ru: 'л', en: 'k' }, { ru: 'д', en: 'l' }, { ru: 'ж', en: ';' }, { ru: 'э', en: '\'' }, { ru: 'Enter', en: 'Enter' }, /*  */ { ru: 'Shift', en: 'Shift' }, { ru: 'я', en: 'z' }, { ru: 'ч', en: 'x' }, { ru: 'с', en: 'c' }, { ru: 'м', en: 'v' }, { ru: 'и', en: 'b' }, { ru: 'т', en: 'n' }, { ru: 'ь', en: 'm' }, { ru: 'б', en: ',' }, { ru: 'ю', en: '.' }, { ru: '.', en: '/' }, { ru: 'ArrowUp', en: 'ArrowUp' }, { ru: 'Shift ', en: 'Shift ' }, /*  */ { ru: 'Control', en: 'Control' }, { ru: 'OS', en: 'OS' }, { ru: 'Alt', en: 'Alt' }, { ru: 'space', en: 'space' }, { ru: 'Alt', en: 'Alt' }, { ru: 'ArrowLeft', en: 'ArrowLeft' }, { ru: 'ArrowDown', en: 'ArrowDown' }, { ru: 'ArrowRight', en: 'ArrowRight' }, { ru: 'Control', en: 'Control' }];
 
 const Keyboard = {
   elements: {
@@ -70,12 +70,17 @@ const Keyboard = {
       }
       const keyElement = document.createElement('button');
       keyElement.setAttribute('type', 'button');
-      keyElement.setAttribute('data-value', `Key${(el.en).toUpperCase()}`);
       keyElement.classList.add('keyboard__key');
-      const insertLineBreak = ['backspace', 'del', 'enter', 'shift '].indexOf(key) !== -1;
+      const insertLineBreak = ['Backspace', 'Delete', 'Enter', 'Shift '].indexOf(key) !== -1;
 
       document.addEventListener('keydown', (event) => {
-        if (event.code === `Key${(el.en).toUpperCase()}`) {
+        if ((event.key).toLowerCase() === (el.en).toLowerCase()) {
+          keyElement.classList.add('--pushed');
+          setTimeout(() => {
+            keyElement.classList.remove('--pushed');
+          }, 200);
+        }
+        if ((event.code).toLowerCase() === (el.en).toLowerCase()) {
           keyElement.classList.add('--pushed');
           setTimeout(() => {
             keyElement.classList.remove('--pushed');
@@ -89,9 +94,10 @@ const Keyboard = {
         if (key === genkey) {
           keyElement.classList.add('general__key');
         }
+
       });
       switch (key) {
-        case 'backspace':
+        case 'Backspace':
           keyElement.classList.add('wide2x');
           keyElement.textContent = '←  Backspace';
           keyElement.addEventListener('click', () => {
@@ -102,7 +108,7 @@ const Keyboard = {
 
           break;
 
-        case 'capslock':
+        case 'CapsLock':
           keyElement.classList.add('wide2x', '--activatable', 'caps');
           keyElement.textContent = key;
 
@@ -112,35 +118,70 @@ const Keyboard = {
           });
 
           break;
-        case 'shift':
-        case 'shift ':
+        case 'Shift':
+        case 'Shift ':
           keyElement.classList.add('wide2x');
           keyElement.textContent = key;
           keyElement.addEventListener('click', (e) => {
             e.preventDefault();
           });
           break;
-        case 'del':
-        case 'alt':
-        case 'ctrl':
-        case 'win':
-        case '◄':
-        case '▲':
-        case '►':
-        case '▼':
+        case 'ArrowLeft':
+          keyElement.textContent = '←';
+          keyElement.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
+          break;
+        case 'ArrowUp':
+          keyElement.textContent = '↑';
+          keyElement.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
+          break;
+        case 'ArrowRight':
+          keyElement.textContent = '→';
+          keyElement.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
+          break;
+        case 'ArrowDown':
+          keyElement.textContent = '↓';
+          keyElement.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
+          break;
+        case 'Delete':
+          keyElement.textContent = 'Del';
+          keyElement.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
+          break;
+        case 'Alt':
           keyElement.textContent = key;
           keyElement.addEventListener('click', (e) => {
             e.preventDefault();
           });
           break;
-        case 'tab':
+        case 'Control':
+          keyElement.textContent = 'Ctrl';
+          keyElement.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
+          break;
+        case 'OS':
+          keyElement.textContent = 'Win';
+          keyElement.addEventListener('click', (e) => {
+            e.preventDefault();
+          });
+          break;
+        case 'Tab':
           keyElement.textContent = key;
           keyElement.addEventListener('click', () => {
             this.properties.value += '  ';
             textArea.value = this.properties.value;
           });
           break;
-        case 'enter':
+        case 'Enter':
           keyElement.classList.add('wide2x');
           keyElement.textContent = key;
           keyElement.addEventListener('click', () => {
